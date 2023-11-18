c56e7d5f9889743f6d486598bdcebe9da5fb746c
Ted <30739356+Tedtanium@users.noreply.github.com>
2023-11-17 17:33:21 -0800

    Delete cacti directory

--- cacti/cacti-client-snmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Extremely simple code to enable net-snmp on devices. Required for Cacti to monitor system performance. Works for both CentOS and Ubuntu.
-
-yum install -y net-snmp
-apt-get install snmp -y
-
-systemctl enable snmpd
-systemctl start snmpd
