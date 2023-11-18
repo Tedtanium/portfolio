530d07aea8e8be6314a68dbff7cc63262db8b04c
Ted <30739356+Tedtanium@users.noreply.github.com>
2023-11-17 17:33:12 -0800

    Delete ldap directory

--- ldap/ldap-client-centos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#This enables any CentOS machine to authenticate as an LDAP client. "LDAPIP" needs to be replaced with the internal IP address of the LDAP server.
-
-yum install -y nss-pam-ldapd nscd
-
-authconfig --enableldap --enableldapauth --ldapserver=ldap://LDAPIP/ --ldapbasedn="dc=capstone,dc=local" --updateall 
