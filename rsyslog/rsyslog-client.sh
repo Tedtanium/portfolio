15a3b6c370f920fdda4a76b90bc19c011015d32e
Ted <30739356+Tedtanium@users.noreply.github.com>
2023-11-17 17:34:16 -0800

    Delete rsyslog directory

--- rsyslog/rsyslog-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#This scriptlet sends logs to the rsyslog server. RSYSLOGIP must be replaced with the server's internal IP address.
-
-echo "*.info;mail.none;authpriv.none;cron.none   @RSYSLOGIP" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
